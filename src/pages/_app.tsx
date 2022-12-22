491bc920b4289134c6225a2ea0802b5f8dba2d4f
rishabh.jain@appinventiv.com <rishabh.jain@appinventiv.com>
2022-12-22 22:32:25 +0530

    feat: redux init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import type { AppProps } from 'next/app';
 import { IntlProvider } from 'src/utils/reactIntl';
 import English from 'src/lang/en.json';
 import French from 'src/lang/fr.json';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const locale = 'en-US';
 const lang = English;
@@ -9,7 +12,11 @@ const lang = English;
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <IntlProvider locale={locale} messages={lang}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </IntlProvider>
   );
 };
---
+++ src/redux/store.ts
@@ -0,0 +1,34 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  debug: false,
+  key: 'root',
+  keyPrefix: 'v.1',
+  storage,
+  blacklist: [],
+  // add reducer name to persist
+  whitelist: ['auth']
+};
+
+// combine all reducers here
+const rootReducer: any = combineReducers({});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+});
+
+export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
